df92300601f7b9b8dd21fbb2ec82848bf48847d3
lohit-varma <lohitvarma8341@gmail.com>
2025-04-10 16:59:46 +0530

    Add Area of Triangle

---
+++ Operators/Area_Of_A_Triangle.py
@@ -0,0 +1,10 @@
+# Area of Triangle ---> 0.5 * base * height
+
+def Area_of_Triangle(b,h):
+    return 0.5*b*h;
+
+base=float(input("Enter the Base: "))
+height = float(input("Enter the Height: "))
+print(Area_of_Triangle(base,height))
+# print(0.5 * base * height)
+
